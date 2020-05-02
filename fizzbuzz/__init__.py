e382e75467a0c6545effd6d5e9027801fffc879d
Pimon <k.arkw87@gmail.com>
2020-05-02 21:04:28 +0900

    First commit

--- fizzbuzz/__init__.py
+++ fizzbuzz/__init__.py
@@ -1,6 +1,10 @@
 def fizzbuzz_convert(number):
+    if number % 3 and 5 == 0:
+        return 'Fizzbuzz'
+
     if number % 3 == 0:
         return 'Fizz'
-
+    if number % 5 == 0:
+        return 'Buzz'
 
 assert fizzbuzz_convert(3) == 'Fizz'
